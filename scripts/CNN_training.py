e8c11d3f884ab402c144cc6ee3dbd82d71561eb8
walterieux <david.desboeufs@hotmail.fr>
2020-11-16 16:16:06 +0100

    sigmoid

--- scripts/CNN_training.py
+++ scripts/CNN_training.py
@@ -6,6 +6,7 @@ import numpy as np
 import tensorflow as tf
 import imageio
 import glob
+from PIL import Image
 
 from tensorflow.keras import datasets, layers, models
 
@@ -14,13 +15,27 @@ def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
 
-def extract_image(image_path, max_value):
-    """Extract all images from 'image_path', then divide all values by max_value"""
+def extract_image(image_path):
+    """
+    Extract all images from 'image_path'
+    All values are between 0 and 1
+    """
 
     imgs = []
     for img_path in glob.glob(image_path + "/*.png"):
         img = imageio.imread(img_path)
-        img = img / max_value
+        img = img / 255
+        imgs.append(img)
+
+    return np.asarray(imgs)
+
+
+def extract_labels(label_path):
+    """Extract all labels from 'label_path'"""
+
+    imgs = []
+    for img_path in glob.glob(label_path + "/*.png"):
+        img = imageio.imread(img_path)
         imgs.append(img)
 
     return np.asarray(imgs)
@@ -30,10 +45,10 @@ data_dir = '../data/'
 train_data_filename = data_dir + 'training/images/'
 train_labels_filename = data_dir + 'training/groundtruth/'
 
-images = extract_image(train_data_filename, 255)
-print(images.shape)
-labels = extract_image(train_labels_filename, 1)
+images = extract_image(train_data_filename)
 
+labels = extract_labels(train_labels_filename)
+labels = labels.reshape(labels.shape[0], -1)
 
 train_images = images[:90]
 test_images = images[90:]
@@ -46,25 +61,25 @@ model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(400, 400, 3)
 model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.Conv2D(64, (3, 3), activation='sigmoid'))
 
 model.summary()
 
 model.add(layers.Flatten())
-model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(1))
+model.add(layers.Dense(64, activation='hard_sigmoid'))  # Change softmax
+model.add(layers.Dense(160000))
 
 model.summary()
 
 model.compile(optimizer='adam',
               loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
-              metrics=['accuracy'])
+              metrics=['binary_accuracy'])
 
-history = model.fit(train_images, train_labels, epochs=4,
+history = model.fit(train_images, train_labels, epochs=2,
                     validation_data=(test_images, test_labels))
 
 test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=1)
-print(test_acc)
+print("Accuracy = ", test_acc)
 
-acc = history.history['accuracy']
-print(acc)
+# acc = history.history['accuracy']
+# print(acc)
