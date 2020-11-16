bde90414dd2b157c686687e251bcbe5acdbf67aa
walterieux <david.desboeufs@hotmail.fr>
2020-11-16 12:00:33 +0100

    extract image

--- scripts/CNN_training.py
+++ scripts/CNN_training.py
@@ -2,144 +2,69 @@ import os
 import subprocess
 import sys
 
-import numpy
+import numpy as np
 import tensorflow as tf
-import mpimg
+import imageio
+import glob
 
-from keras import models, layers
-
-from scripts.tf_aerial_images import extract_data, extract_labels
+from tensorflow.keras import datasets, layers, models
 
 
 def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
 
-IMG_PATCH_SIZE = 16
+def extract_image(image_path, max_value):
+    """Extract all images from 'image_path', then divide all values by max_value"""
 
-
-# taken from tf_arial
-def img_crop(im, w, h):
-    list_patches = []
-    imgwidth = im.shape[0]
-    imgheight = im.shape[1]
-    is_2d = len(im.shape) < 3
-    for i in range(0, imgheight, h):
-        for j in range(0, imgwidth, w):
-            if is_2d:
-                im_patch = im[j:j+w, i:i+h]
-            else:
-                im_patch = im[j:j+w, i:i+h, :]
-            list_patches.append(im_patch)
-    return list_patches
-
-
-# taken from tf_arial
-def value_to_class(v):
-    foreground_threshold = 0.25  # percentage of pixels > 1 required to assign a foreground label to a patch
-    df = numpy.sum(v)
-    if df > foreground_threshold:  # road
-        return [0, 1]
-    else:  # bgrd
-        return [1, 0]
-
-
-# taken from tf_arial
-def extract_labels(filename, num_images):
-    """Extract the labels into a 1-hot matrix [image index, label index]."""
-    gt_imgs = []
-    for i in range(1, num_images + 1):
-        imageid = "satImage_%.3d" % i
-        image_filename = filename + imageid + ".png"
-        if os.path.isfile(image_filename):
-            print('Loading ' + image_filename)
-            img = mpimg.imread(image_filename)
-            gt_imgs.append(img)
-        else:
-            print('File ' + image_filename + ' does not exist')
-
-    num_images = len(gt_imgs)
-    gt_patches = [img_crop(gt_imgs[i], IMG_PATCH_SIZE, IMG_PATCH_SIZE) for i in range(num_images)]
-    data = numpy.asarray([gt_patches[i][j] for i in range(len(gt_patches)) for j in range(len(gt_patches[i]))])
-    labels = numpy.asarray([value_to_class(numpy.mean(data[i])) for i in range(len(data))])
-
-    # Convert to dense 1-hot representation.
-    return labels.astype(numpy.float32)
-
-
-# taken from tf_arial
-def extract_data(filename, num_images):
-    """Extract the images into a 4D tensor [image index, y, x, channels].
-    Values are rescaled from [0, 255] down to [-0.5, 0.5].
-    """
     imgs = []
-    for i in range(1, num_images + 1):
-        imageid = "satImage_%.3d" % i
-        image_filename = filename + imageid + ".png"
-        if os.path.isfile(image_filename):
-            print('Loading ' + image_filename)
-            img = mpimg.imread(image_filename)
-            imgs.append(img)
-        else:
-            print('File ' + image_filename + ' does not exist')
+    for img_path in glob.glob(image_path + "/*.png"):
+        img = imageio.imread(img_path)
+        img = img / max_value
+        imgs.append(img)
 
-    num_images = len(imgs)
-    IMG_WIDTH = imgs[0].shape[0]
-    IMG_HEIGHT = imgs[0].shape[1]
-    N_PATCHES_PER_IMAGE = (IMG_WIDTH / IMG_PATCH_SIZE) * (IMG_HEIGHT / IMG_PATCH_SIZE)
-
-    img_patches = [img_crop(imgs[i], IMG_PATCH_SIZE, IMG_PATCH_SIZE) for i in range(num_images)]
-    data = [img_patches[i][j] for i in range(len(img_patches)) for j in range(len(img_patches[i]))]
-
-    return numpy.asarray(data)
+    return np.asarray(imgs)
 
 
-def main(argv=None):
-    install('mpimg')
-    data_dir = '../data/'
-    train_data_filename = data_dir + 'training/images/'
-    train_labels_filename = data_dir + 'training/groundtruth/'
-    TRAINING_SIZE = 100
+data_dir = '../data/'
+train_data_filename = data_dir + 'training/images/'
+train_labels_filename = data_dir + 'training/groundtruth/'
 
-    data = extract_data(train_data_filename, TRAINING_SIZE)
-    labels = extract_labels(train_labels_filename, TRAINING_SIZE)
-
-    print(data)
-
-    train_images = data[:90]
-    test_images = data[90:]
-
-    train_labels = labels[:90]
-    test_labels = labels[90:]
-
-    model = models.Sequential()
-    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(400, 400, 3)))
-    model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-    model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-
-    model.summary()
-
-    model.add(layers.Flatten())
-    model.add(layers.Dense(64, activation='relu'))
-    model.add(layers.Dense(1))
-
-    model.summary()
-
-    model.compile(optimizer='adam',
-                  loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
-                  metrics=['accuracy'])
-
-    history = model.fit(train_images, train_labels, epochs=4,
-                        validation_data=(test_images, test_labels))
-
-    test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=1)
-    print(test_acc)
-
-    acc = history.history['accuracy']
-    print(acc)
+images = extract_image(train_data_filename, 255)
+print(images.shape)
+labels = extract_image(train_labels_filename, 1)
 
 
-if __name__ == '__main__':
-    tf.app.run()
+train_images = images[:90]
+test_images = images[90:]
+
+train_labels = labels[:90]
+test_labels = labels[90:]
+
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(400, 400, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+
+model.summary()
+
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(1))
+
+model.summary()
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+history = model.fit(train_images, train_labels, epochs=4,
+                    validation_data=(test_images, test_labels))
+
+test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=1)
+print(test_acc)
+
+acc = history.history['accuracy']
+print(acc)
