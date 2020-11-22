489a10d980ae240a74d57fdb45f34b8415e6685c
Jean Gillain <jeangillain@homtail.com>
2020-11-22 20:02:32 +0100

    changement appliqué

--- scripts/images_preproces.py
+++ scripts/images_preproces.py
@@ -4,20 +4,18 @@ Created on Tue Nov 17 18:09:52 2020
 
 @author: jeang
 """
-from scipy.ndimage import rotate
-from scipy.misc import imread, imshow
-import scipy.misc
-#from cnn_training import * 
+
 import numpy as np 
 from scipy.ndimage import filters,zoom
 from PIL import Image 
 import os
 import imageio
-""" @input: filename: name of the folder where the images are stored 
+
+def read_images(filename, num_images):
+    """ @input: filename: name of the folder where the images are stored 
             num_images: number of images stored in the folder 
     @output: list of images stored in the folder
 """
-def read_images(filename, num_images):
     imgs = []
     for i in range(1, num_images + 1):
         imageid = "satImage_%.3d" % i
@@ -28,11 +26,12 @@ def read_images(filename, num_images):
     return imgs
 
 
-""" @input: filename: name of the folder where the images will bestored 
-            data_images: list of images that will be rotated of [45,90,135..] degrees 
+
+def rotate_images(filename,data_images): 
+    """ @input: filename: name of the folder where the images will bestored 
+            data_images: list of images that will be rotated of [45,90,135..,360] degrees 
     @output: images rotated stored in the folder
 """
-def rotate_images(filename,data_images): 
     number=0
     for i,image in enumerate(data_images):
         for j in range(8):
@@ -42,26 +41,26 @@ def rotate_images(filename,data_images):
                 rotate_img = rotate_img.resize((400, 400)) 
             save_img(filename,rotate_img,number+j+1)
         number+=8
-""" @input: filename: name of the folder where the images will bestored 
+def edges_images(filename,data_images):
+    """ @input: filename: name of the folder where the images will bestored 
             data_images: list of images that we will get the edges
     @output: images edges in the folder
 """
-def edges_images(filename,data_images):
+
     for j,image in enumerate(data_images):
         image=np.array( image, dtype='uint32' )
-        imx = np.zeros(image.shape)
-        imy = np.zeros(image.shape)
         filters.sobel(image,1,imx,cval=0.0)  # axis 1 is x
         filters.sobel(image,0,imy, cval=0.0) # axis 0 is y
         magnitude = np.sqrt(imx**2+imy**2)
         save_img(filename,np.where(magnitude>=0.16*np.max(magnitude),255,0),j+1)
         
-""" @input: filename: name of the folder where the images will bestored 
+
+def save_img(filename,image,number):
+    """ @input: filename: name of the folder where the images will bestored 
             image: image that will be stored (array)
             number: the image will be stored with filename  filename + imageid + ".png"
     @output: image  in the folder
 """
-def save_img(filename,image,number):
     imageid = "satImage_%.3d" % number
     image_filename = filename + imageid + ".png"
     imageio.imwrite(image_filename, image)
@@ -80,7 +79,6 @@ train_data_filename_edges_rotated = data_dir + 'training/images_edges_rotated/'
 
 
 data = read_images(train_data_filename, TRAINING_SIZE)
-#rotate_images(train_data_filename_rotated,data)
 data_rotated = read_images(train_data_filename_rotated,800)
 
 data_groundtruh = read_images(train_labels_filename,TRAINING_SIZE)
