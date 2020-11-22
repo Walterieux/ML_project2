e40e0c3438eb7f27f9c082adf1f7861c6fd28637
Jean Gillain <jeangillain@homtail.com>
2020-11-22 17:43:48 +0100

    rotation images + edges format (400, 400)

--- scripts/cnn_training.py
+++ scripts/cnn_training.py
@@ -1,149 +1,148 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Nov 16 10:19:28 2020
-
-@author: jeang
-"""
-
 import os
 import subprocess
 import sys
 
-import numpy
+import numpy as np
 import tensorflow as tf
-import mpimg as mpimg
-import scipy.misc
-from keras import models, layers
+import ipykernel
+import imageio
+import glob
+from PIL import Image
+from patchify import patchify, unpatchify
+from sklearn.model_selection import train_test_split
 
+from tensorflow.keras import layers, models
+
+config = tf.compat.v1.ConfigProto(gpu_options=tf.compat.v1.GPUOptions(per_process_gpu_memory_fraction=0.8))
+config.gpu_options.allow_growth = True
+session = tf.compat.v1.Session(config=config)
+tf.compat.v1.keras.backend.set_session(session)
 
 
 def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
 
-IMG_PATCH_SIZE = 16
-
-
-# taken from tf_arial
-def img_crop(im, w, h):
-    list_patches = []
-    imgwidth = im.shape[0]
-    imgheight = im.shape[1]
-    is_2d = len(im.shape) < 3
-    for i in range(0, imgheight, h):
-        for j in range(0, imgwidth, w):
-            if is_2d:
-                im_patch = im[j:j+w, i:i+h]
-            else:
-                im_patch = im[j:j+w, i:i+h, :]
-            list_patches.append(im_patch)
-    return list_patches
-
-
-# taken from tf_arial
-def value_to_class(v):
-    foreground_threshold = 0.25  # percentage of pixels > 1 required to assign a foreground label to a patch
-    df = numpy.sum(v)
-    if df > foreground_threshold:  # road
-        return [0, 1]
-    else:  # bgrd
-        return [1, 0]
-
-
-# taken from tf_arial
-def extract_labels(filename, num_images):
-    """Extract the labels into a 1-hot matrix [image index, label index]."""
-    gt_imgs = []
-    for i in range(1, num_images + 1):
-        imageid = "satImage_%.3d" % i
-        image_filename = filename + imageid + ".png"
-        if os.path.isfile(image_filename):
-            print('Loading ' + image_filename)
-            img = scipy.misc.imread(image_filename)
-            gt_imgs.append(img)
-        else:
-            print('File ' + image_filename + ' does not exist')
-
-    num_images = len(gt_imgs)
-    gt_patches = [img_crop(gt_imgs[i], IMG_PATCH_SIZE, IMG_PATCH_SIZE) for i in range(num_images)]
-    data = numpy.asarray([gt_patches[i][j] for i in range(len(gt_patches)) for j in range(len(gt_patches[i]))])
-    labels = numpy.asarray([value_to_class(numpy.mean(data[i])) for i in range(len(data))])
-
-    # Convert to dense 1-hot representation.
-    return labels.astype(numpy.float32)
-
-
-# taken from tf_arial
-def extract_data(filename, num_images):
-    """Extract the images into a 4D tensor [image index, y, x, channels].
-    Values are rescaled from [0, 255] down to [-0.5, 0.5].
+def extract_images(image_path):
     """
+    Extract all images from 'image_path'
+    All values are between 0 and 1
+    """
+
     imgs = []
-    for i in range(1, num_images + 1):
-        imageid = "satImage_%.3d" % i
-        image_filename = filename + imageid + ".png"
-        if os.path.isfile(image_filename):
-            print('Loading ' + image_filename)
-            img = scipy.misc.imread(image_filename)
-            imgs.append(img)
+    for img_path in glob.glob(image_path + "/*.png"):
+        img = imageio.imread(img_path)
+        img = img / 255.0
+        imgs.append(img)
+
+    return np.asarray(imgs)
+
+
+def extract_labels(label_path):
+    """Extract all labels from 'label_path'"""
+
+    imgs = []
+    for img_path in glob.glob(label_path + "/*.png"):
+        img = imageio.imread(img_path)
+        imgs.append(img)
+
+    return np.asarray(imgs)
+
+
+def create_patches(data, patch_shape):
+    """separate image into patches, data is a collection of images"""
+    imgs = []
+    for i in range(data.shape[0]):
+        if len(patch_shape) == 3:  # RGB images
+            patches = patchify(data[i], patch_shape, step=patch_shape[0])
+            patches = patches.reshape((-1, patch_shape[0], patch_shape[1], patch_shape[2]))
+            imgs.extend(patches)
         else:
-            print('File ' + image_filename + ' does not exist')
+            patches = patchify(data[i], patch_shape, step=patch_shape[0])
+            patches = patches.reshape((-1, patch_shape[0], patch_shape[1]))
+            number_of_patches = patches.shape[0]
+            patches = patches.reshape((number_of_patches, -1))
+            imgs.extend(patches)
 
-    num_images = len(imgs)
-    IMG_WIDTH = imgs[0].shape[0]
-    IMG_HEIGHT = imgs[0].shape[1]
-    N_PATCHES_PER_IMAGE = (IMG_WIDTH / IMG_PATCH_SIZE) * (IMG_HEIGHT / IMG_PATCH_SIZE)
-
-    img_patches = [img_crop(imgs[i], IMG_PATCH_SIZE, IMG_PATCH_SIZE) for i in range(num_images)]
-    data = [img_patches[i][j] for i in range(len(img_patches)) for j in range(len(img_patches[i]))]
-
-    return numpy.asarray(data)
+    return np.asarray(imgs)
 
 
+def main():
+    install("patchify")
+    img_patch_size = 20
+    img_shape = (400, 400)
 
-"""install('mpimg')
-data_dir = '../data/'
-train_data_filename = data_dir + 'training/images/'
-train_labels_filename = data_dir + 'training/groundtruth/'
-TRAINING_SIZE = 100
+    data_dir = '../data/'
+    train_data_filename = data_dir + 'training/images/'
+    train_labels_filename = data_dir + 'training/groundtruth/'
 
-data = extract_data(train_data_filename, TRAINING_SIZE)
-labels = extract_labels(train_labels_filename, TRAINING_SIZE)
+    # Retrieve images/groundtruth and create mini patches
+    images = extract_images(train_data_filename)
+    labels = extract_labels(train_labels_filename)
+
+    # Split data
+    train_images, test_images, train_labels, test_labels = train_test_split(images, labels, test_size=0.1)
+
+    # create mini_patches
+    train_images = create_patches(train_images, (img_patch_size, img_patch_size, 3))
+    test_images = create_patches(test_images, (img_patch_size, img_patch_size, 3))
+    train_labels = create_patches(train_labels, (img_patch_size, img_patch_size))
+    test_labels = create_patches(test_labels, (img_patch_size, img_patch_size))
+
+    # Test: retrieve original train labels using unpatchify
+    test_labels_original = test_labels.reshape(
+        (-1, int(img_shape[0] / img_patch_size), int(img_shape[1] / img_patch_size), img_patch_size, img_patch_size))
+    print("test lab origi: ", test_labels_original.shape)
+    test_labels_original_image0 = unpatchify(test_labels_original[0], img_shape)
+
+    # CNN
+    model = models.Sequential()
+    model.add(
+        layers.Conv2D(64, kernel_size=(3, 3), activation='relu', input_shape=(img_patch_size, img_patch_size, 3)))
+    model.add(layers.Conv2D(128, kernel_size=(3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(64, kernel_size=(3, 3), activation='relu'))
+
+    model.add(layers.Flatten())
+    model.add(layers.Dense(256, activation='relu'))
+    model.add(layers.Dense(img_patch_size ** 2, activation='sigmoid'))
+
+    model.summary()
+
+    model.compile(optimizer='adam',
+                  loss='mse',
+                  metrics=['accuracy'])
+
+    model.fit(train_images, train_labels, epochs=2)
+
+    test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=1)
+    print("Accuracy = ", test_acc)
+
+    # prediction
+    prediction = model.predict(test_images)
+
+    # show expected and predicted image
+    prediction_reshaped = prediction.reshape(
+        (-1, int(img_shape[0] / img_patch_size), int(img_shape[1] / img_patch_size), img_patch_size, img_patch_size))
+    prediction_reshaped = unpatchify(prediction_reshaped[0], img_shape)
+    comparator = np.concatenate(
+        ((test_labels_original_image0 * 255).astype('uint8'), (prediction_reshaped * 255).astype('uint8')), axis=1)
+    img = Image.fromarray(comparator)
+    img.show()
+
+    # Testing
+    tot_black_pixels = np.sum(test_labels < 0.5)
+    tot_pixels = test_labels.shape[0] * test_labels.shape[1]
+    print(tot_black_pixels / tot_pixels * 100, "% of black pixels")
+
+    """
+    threshold = 0.5
+    result[result > threshold] = 1
+    result[result <= threshold] = 0
+    img = Image.fromarray(result * 255, 'L')
+    img.show()
+    """
 
 
-train_images = data[:90]
-test_images = data[90:]
-
-train_labels = labels[:90]
-test_labels = labels[90:]
-
-model = models.Sequential()
-model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(400, 400, 3)))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-
-model.summary()
-
-model.add(layers.Flatten())
-model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(1))
-
-model.summary()
-
-model.compile(optimizer='adam',
-              loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
-              metrics=['accuracy'])
-
-history = model.fit(train_images, train_labels, epochs=4,
-                    validation_data=(test_images, test_labels))
-
-test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=1)
-print(test_acc)
-
-acc = history.history['accuracy']
-print(acc)"""
-
-
-
+if __name__ == '__main__':
+    main()
--- scripts/images_preproces.py
+++ scripts/images_preproces.py
@@ -7,61 +7,63 @@ Created on Tue Nov 17 18:09:52 2020
 from scipy.ndimage import rotate
 from scipy.misc import imread, imshow
 import scipy.misc
-from cnn_training import * 
+from CNN_training import * 
 import numpy as np 
 from scipy.ndimage import filters,zoom
 from PIL import Image 
-
+import imageio
+""" @input: -filename: name of the directory where the images are stored 
+            -num_images: number of images in the directory
+    @output: list of 3-d array (RGB) of images 
+"""
 def read_images(filename, num_images):
-    """Extract the images into a 4D tensor [image index, y, x, channels].
-    Values are rescaled from [0, 255] down to [-0.5, 0.5].
-    """
+    
     imgs = []
     for i in range(1, num_images + 1):
         imageid = "satImage_%.3d" % i
         image_filename = filename + imageid + ".png"
         if os.path.isfile(image_filename):
-            print('Loading ' + image_filename)
             img = Image.open(image_filename)
-            #img = scipy.misc.imread(image_filename)
             imgs.append(img)
     return imgs
+
+""" @input : -filename : name of the directory where the images should be stored 
+             -data_images: array of images that will be rotated from [45,90,135,..360] degrees
+    @output: store the rotated images in the directory filename
+"""
 def rotate_images(filename,data_images): 
     number=0
     for i,image in enumerate(data_images):
-        for j in range(6):
+        for j in range(8):
             rotate_img_1 = image.rotate( (j+1)*45)
             if ((j+1)*45) % 90 != 0 :
-                height=400
-                #print(np.shape(rotate_img_1))
-                left = 4
-                top = height / 5
-                right = 154
-                bottom = 3 * height / 5
-  
-# Cropped image of above dimension  
-# (It will not change orginal image)  
-                rotate_img_1 = rotate_img_1.crop((left, top, right, bottom)) 
-                newsize = (400, 400) 
-                rotate_img_1 = rotate_img_1.resize(newsize) 
-                #print("after" ,np.shape(rotate_img_1))
-            save_img(filename,rotate_img_1,number+j)
-        number+=8
+                rotate_img_1 = rotate_img_1.crop(( 60, 60, 340, 340))  
+                rotate_img_1 = rotate_img_1.resize((400, 400) ) 
 
+            save_img(filename,rotate_img_1,number+j+1)
+        number+=8
+""" @input : -filename : name of the directory where the images should be stored 
+             -data_images: array of images 
+    @output: store the edges of the images in the directory filename
+"""
 def edges_images(filename,data_images):
     for j,image in enumerate(data_images):
+        image=np.array( image, dtype='uint32' )
         imx = np.zeros(image.shape)
         imy = np.zeros(image.shape)
         filters.sobel(image,1,imx,cval=0.0)  # axis 1 is x
         filters.sobel(image,0,imy, cval=0.0) # axis 0 is y
         magnitude = np.sqrt(imx**2+imy**2)
-        save_img(filename,np.where(magnitude>=0.16*np.max(magnitude),255,0),j)
-        
-
+        save_img(filename,np.where(magnitude>=0.16*np.max(magnitude),255,0),j+1)
+    
+""" @input : -filename : name of the directory where the image should be stored 
+             -image: image object
+             -number: the image will be stored in the directory with filemane  "satImage_%.3d" % number
+"""
 def save_img(filename,image,number):
     imageid = "satImage_%.3d" % number
     image_filename = filename + imageid + ".png"
-    scipy.misc.imsave(image_filename, image)
+    imageio.imwrite(image_filename, image)
 
 
 data_dir = '../data/'
@@ -77,13 +79,12 @@ train_data_filename_edges_rotated = data_dir + 'training/images_edges_rotated/'
 
 
 data = read_images(train_data_filename, TRAINING_SIZE)
-rotate_images(train_data_filename_rotated,data)
-#data_rotated = read_images(train_data_filename_rotated,300)
-
-#data_groundtruh = read_images(train_labels_filename,TRAINING_SIZE)
+#rotate_images(train_data_filename_rotated,data)
+data_rotated = read_images(train_data_filename_rotated,800)
+print(len(data_rotated))
+data_groundtruh = read_images(train_labels_filename,TRAINING_SIZE)
 #rotate_images(train_data_filename_rotated_groundtruth,data_groundtruh)
 
-#rotate_images(train_data_filename_rotated,data)
 
-#edges_images(train_data_filename_edges,data)
-#edges_images(train_data_filename_edges_rotated,data_rotated)
+edges_images(train_data_filename_edges,data)
+edges_images(train_data_filename_edges_rotated,data_rotated)
