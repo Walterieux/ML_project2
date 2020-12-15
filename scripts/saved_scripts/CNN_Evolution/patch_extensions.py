6474c253c9c3379f5000f5783636b4f2e55aca32
Mathieu Caboche <43415366+m-caboche@users.noreply.github.com>
2020-12-15 17:39:08 +0100

    remove stuff

--- scripts/saved_scripts/CNN_Evolution/patch_extensions.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# TODO : CLEAN and run for test
-# TODO : take from CNN_training.py, run with border and no border
-# TODO : output : graph original and graph with border
-# TODO : CAREFUL WITH PATHS
